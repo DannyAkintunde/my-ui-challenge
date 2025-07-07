5099f180d97cfc143902a4edeb8655e22aecd017
DannyAkintunde <dannyakintundelct@gmail.com>
2025-07-07 12:58:51 +0100

    clean up touro and dependecies update

--- touro/svg-loader.js
+++ touro/svg-loader.js
@@ -3,15 +3,19 @@
 const iconRoot = new URL("touro/assets", window.origin);
 
 async function fetchIcon(iconName) {
-    const result = await fetch(iconRoot.toString() + `/${iconName}.svg`);
+    const result = await fetch(iconRoot.toString() + `icons/${iconName}.svg`);
     return await result.text();
 }
 
-document.querySelectorAll(".icon").forEach(async (iconPlaceholder) => {
-    const iconName = iconPlaceholder.dataset.icon;
-    const iconData = await fetchIcon(iconName);
-    console.log(iconData);
-    const icon = document.createElement("div");
-    icon.innerHTML = iconData;
-    iconPlaceholder.replaceWith(...icon.childNodes);
-});
+function loadIcons() {
+    document.querySelectorAll(".icon").forEach(async (iconPlaceholder) => {
+        const iconName = iconPlaceholder.dataset.icon;
+        const iconData = await fetchIcon(iconName);
+        // console.log(iconData);
+        const icon = document.createElement("div");
+        icon.innerHTML = iconData;
+        iconPlaceholder.replaceWith(...icon.childNodes);
+    });
+}
+
+loadIcons();
