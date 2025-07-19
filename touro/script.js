104128f3767d94d57a0297c08d6d8bf91b0575b3
DannyAkintunde <dannyakintundelct@gmail.com>
2025-07-19 13:13:30 +0100

    impliment popular section

--- touro/script.js
+++ touro/script.js
@@ -4,12 +4,46 @@ const mobileNavToggle = document.querySelector("#menu-toggle");
 let navOpen = false;
 const openMenu = (e, close) => {
     navOpen = mobileNav.classList.toggle("active", close);
-    if (navOpen)
+    if (navOpen) {
+        mobileNav.ariaHidden = false;
         mobileNavToggle.innerHTML = `<i class="icon" data-icon="menu-close">`;
-    else mobileNavToggle.innerHTML = `<i class="icon" data-icon="menu">`;
+    } else {
+        mobileNav.ariaHidden = true;
+        mobileNavToggle.innerHTML = `<i class="icon" data-icon="menu">`;
+    }
 };
 
 document.addEventListener("click", (e) => {
     if (mobileNavToggle.contains(e.target)) openMenu(e);
     else if (navOpen && !mobileNav.contains(e.target)) openMenu(e, false);
+});
+
+const tabbers = document.querySelectorAll(".tabber");
+
+tabbers.forEach((tabber) => {
+    const tabList = tabber.querySelector(".tabber__tabs");
+    const tabPanels = tabber.querySelector(".tabber__tabpanels");
+    let activeTab = tabList.querySelector(".active");
+
+    tabList.addEventListener("click", (e) => {
+        if (
+            e.target.classList.contains("tabber__tab") &&
+            !activeTab.contains(e.target)
+        ) {
+            //deactivate tab
+            activeTab.classList.toggle("active", false);
+            activeTab.ariaSelected = false;
+            tabPanels
+                .querySelector(`[data-index="${activeTab.dataset.index}"]`)
+                .classList.toggle("active", false);
+
+            // activate tab
+            e.target.classList.toggle("active", true);
+            e.target.ariaSelected = true;
+            tabPanels
+                .querySelector(`[data-index="${e.target.dataset.index}"]`)
+                .classList.toggle("active", true);
+            activeTab = e.target;
+        }
+    });
 });
